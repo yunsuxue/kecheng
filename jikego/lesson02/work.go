6fe70f0ac390609c2ae47d1157d6a54d947df176
xiaoqing9 <xiaoqing9@staff.weibo.com>
2021-04-25 21:02:00 +0800

    add message

--- jikego/lesson02/work.go
+++ jikego/lesson02/work.go
@@ -10,7 +10,7 @@ func main() {
 	//错误应该调用方处理, 除非错误无关紧要,记下日志,逻辑继续执行,db错误应该是重要错误,调用方需要处理
 	result, err := dao.UpdateProduct()
 	if err != nil {
-		log.Fatal("db 执行失败, 错误, %v", err)
+		log.Fatalf("db 执行失败, 错误: %v", err)
 	}
 
 	if result != nil {
