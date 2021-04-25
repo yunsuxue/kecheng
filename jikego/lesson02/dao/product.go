36bf9ba4c70616c8ec1f818045b50b13369cb397
xiaoqing9 <xiaoqing9@staff.weibo.com>
2021-04-25 21:29:33 +0800

    add message

--- jikego/lesson02/dao/product.go
+++ jikego/lesson02/dao/product.go
@@ -7,33 +7,30 @@ import (
 	"runtime"
 )
 
-var codePath string
-var codeLine int
-
 //db发生错误应该调用方处理更符合逻辑, 此处可以使用 go 1.13新加的%w, 加上错误信息,让链路清晰, 或者使用 pkg/errors 包包装
 func UpdateProduct() (sql.Result, error) {
 	db, err := sql.Open("mysql", "root:123456@(localhost:3306)/golab?charset=utf8")
 	if err != nil {
-		_, codePath, codeLine, _ = runtime.Caller(0)
-		return nil, fmt.Errorf(" db open error,UpdateProduct fail,path[%s],line[%d]:%w", codePath, codeLine, err)
+		return nil, genErr(" db open error,UpdateProduct fail,path[%s],line[%d]:%w", err)
 	}
 	defer db.Close()
 
 	//插入数据
 	stmt, err := db.Prepare("INSERT INTO userinfo SET username=?,department=?,created=?")
 	if err != nil {
-		//可以加上文件和行号信息
-		_, codePath, codeLine, _ = runtime.Caller(0)
-		return nil, fmt.Errorf(" sql Prepare fail,path[%s],line[%d]:%w", codePath, codeLine, err)
+		return nil, genErr("sql Prepare fail,path[%s],line[%d]:%w", err)
 	}
 
 	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
 	if err != nil {
-		//可以加上文件和行号信息
-		_, codePath, codeLine, _ = runtime.Caller(0)
-		return nil, fmt.Errorf(" sql Exec fail,path[%s],line[%d]:%w", codePath, codeLine, err)
+		return nil, genErr("sql Exec fail,path[%s],line[%d]:%w", err)
 	}
 
 	//...
 	return res, nil
+}
+
+func genErr(err_format string, err error)  error {
+	_, codePath, codeLine, _ := runtime.Caller(1)
+	return fmt.Errorf(err_format, codePath, codeLine, err)
 }
--- jikego/lesson02/work.go
+++ jikego/lesson02/work.go
@@ -10,6 +10,11 @@ func main() {
 	//错误应该调用方处理, 除非错误无关紧要,记下日志,逻辑继续执行,db错误应该是重要错误,调用方需要处理
 	result, err := dao.UpdateProduct()
 	if err != nil {
+		//如果查看路径信息,可以循环unwrap
+		/*for err != nil {
+			fmt.Println(err)
+			err = errors.Unwrap(err)
+		}*/
 		log.Fatalf("db 执行失败, 错误: %v", err)
 	}
 
