d0d354b20361b1ffc11165f2b9dd91a2889a92c0
xiaoqing9 <xiaoqing9@staff.weibo.com>
2021-04-25 20:58:39 +0800

    add message

--- jikego/lesson02/dao/product.go
+++ jikego/lesson02/dao/product.go
@@ -7,13 +7,26 @@ import (
 )
 
 //db发生错误应该调用方处理更符合逻辑, 此处可以使用 go 1.13新加的%w, 加上错误信息,让链路清晰, 或者使用 pkg/errors 包包装
-func UpdateProduct() (bool, error) {
+func UpdateProduct() (sql.Result, error) {
 	db, err := sql.Open("mysql", "root:123456@(localhost:3306)/golab?charset=utf8")
 	if err != nil {
-		return false, fmt.Errorf(" db open error,UpdateProduct fail:%w", err)
+		return nil, fmt.Errorf(" db open error,UpdateProduct fail:%w", err)
 	}
 	defer db.Close()
 
+	//插入数据
+	stmt, err := db.Prepare("INSERT INTO userinfo SET username=?,department=?,created=?")
+	if err != nil {
+		//可以加上文件和行号信息
+		return nil, fmt.Errorf(" sql Prepare fail:%w", err)
+	}
+
+	res, err := stmt.Exec("astaxie", "研发部门", "2012-12-09")
+	if err != nil {
+		//可以加上文件和行号信息
+		return nil, fmt.Errorf(" sql Prepare fail:%w", err)
+	}
+
 	//...
-	return true, nil
+	return res, nil
 }
--- jikego/lesson02/work.go
+++ jikego/lesson02/work.go
@@ -8,12 +8,12 @@ import (
 
 func main() {
 	//错误应该调用方处理, 除非错误无关紧要,记下日志,逻辑继续执行,db错误应该是重要错误,调用方需要处理
-	ok, err := dao.UpdateProduct()
+	result, err := dao.UpdateProduct()
 	if err != nil {
 		log.Fatal("db 执行失败, 错误, %v", err)
 	}
 
-	if ok {
+	if result != nil {
 		//...
 		fmt.Println("db ok")
 	}
